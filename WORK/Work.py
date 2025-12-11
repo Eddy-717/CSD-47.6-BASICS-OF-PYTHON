c1bd3c52c75bb7caf339d6a9b7fbda96628fcf0f
Edward Awutey <edwardawutey@gmail.com>
2025-12-11 13:45:59 +0000

    Login

---
+++ WORK/Work.py
@@ -0,0 +1,15 @@
+playlist =["No war in bang si se", "Passion Fruit", "Teenage Fever", "Get Along better", "Snooze"]
+
+top_artists =("Drake", "21 Savage", "Asa", "Dave" )
+
+my_music = (playlist, top_artists)
+
+print(len(my_music))
+
+print(playlist[2])
+
+playlist[0] = "Ye"
+
+playlist.append("Slow Down")
+
+print(playlist)
